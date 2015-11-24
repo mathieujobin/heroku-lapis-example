3eadc434c4c08b53ebbf4b3025aee5631bd89af3
Mathieu Jobin <mathieu@justbudget.com>
2015-11-24 14:22:35 -0500

    extract DB info from database string

--- config.lua
+++ config.lua
@@ -1,8 +1,37 @@
 local config
 config = require("lapis.config").config
+local database_url = os.getenv("DATABASE_URL")
+local dbinfo
+do
+  local _accum_0 = { }
+  local _len_0 = 1
+  for x in string.gmatch(database_url, "([%u%l%d%-%.]+)") do
+    _accum_0[_len_0] = x
+    _len_0 = _len_0 + 1
+  end
+  dbinfo = _accum_0
+end
+config("development", function()
+  num_workers(1)
+  code_cache("off")
+  port(8080)
+  postgresql_url(database_url)
+  return postgres(function()
+    host(dbinfo[4])
+    user(dbinfo[2])
+    password(dbinfo[3])
+    return database(dbinfo[6])
+  end)
+end)
 return config("heroku", function()
   num_workers(8)
   code_cache("on")
   port(os.getenv("PORT"))
-  return postgresql_url(os.getenv("DATABASE_URL"))
+  postgresql_url(database_url)
+  return postgres(function()
+    host(dbinfo[4])
+    user(dbinfo[2])
+    password(dbinfo[3])
+    return database(dbinfo[6])
+  end)
 end)
--- config.moon
+++ config.moon
@@ -1,8 +1,27 @@
 
 import config from require "lapis.config"
 
+database_url = os.getenv "DATABASE_URL"
+dbinfo = [x for x in string.gmatch(database_url, "([%u%l%d%-%.]+)")]
+
+config "development", ->
+  num_workers 1
+  code_cache "off"
+  port 8080
+  postgresql_url database_url
+  postgres ->
+    host dbinfo[4]
+    user dbinfo[2]
+    password dbinfo[3]
+    database dbinfo[6]
+
 config "heroku", ->
   num_workers 8
   code_cache "on"
   port os.getenv "PORT"
-  postgresql_url os.getenv "DATABASE_URL"
+  postgresql_url database_url
+  postgres ->
+    host dbinfo[4]
+    user dbinfo[2]
+    password dbinfo[3]
+    database dbinfo[6]
